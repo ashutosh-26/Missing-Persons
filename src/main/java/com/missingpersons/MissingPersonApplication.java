76d51414b27d58f129edcb509c4dabeda232c9be
Ashutosh-26 <ashutoshman@gmail.com>
2025-10-30 22:29:21 +0530

    Initial project commit

---
+++ src/main/java/com/missingpersons/MissingPersonApplication.java
@@ -0,0 +1,13 @@
+package com.missingpersons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissingPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissingPersonApplication.class, args);
+	}
+
+}
